7e5d9433b80903ab3135eaeae78d027151d0fe4a
Shreesha Kumar P <shreeshbhat0509@gmail.com>
2026-09-21 22:00:54 +0530

    Add GitHub Actions backend deployment

--- gateway/app.py
+++ gateway/app.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://main.d138fmwwopwc60.amplifyapp.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
